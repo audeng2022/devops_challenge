25f2c775b3028f92e2b278a91ecbf948296c862b
audeng2022 <audeng2022@gmail.com>
2026-01-20 12:17:43 -0500

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,15 +2,13 @@
 # Separate Variables file for reference in main.tf
 
 # Default region is us east 1, can change this to different region optionally
-variable "aws_region" 
-{
+variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
 
 # Optional variable name to give resources created by Terraform, default is devops-challenge
-variable "project_name" 
-{
+variable "project_name" {
   type    = string
   default = "devops-challenge"
 }
