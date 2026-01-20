4f305c85e4dc2c28141755acdd3b28d97e009053
audeng2022 <audeng2022@gmail.com>
2026-01-20 11:53:06 -0500

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,19 @@
-#Output Application Load Balancer DNS Name to be used for testing
+# @Austin Deng
+#
+# Output Application Load Balancer DNS Name to be used for testing
+# Also outputs ecr repository name and name tag for input into Github variables in step 2
 
 output "alb_dns_name" 
 {
   value = aws_lb.app_alb.dns_name
+}
+
+output "ecr_repository_name"
+{
+  value = "${var.project_name}-repo"
+}
+
+output "ecr_name_tag"
+{
+  value = "${var.project_name}-ec2"
 }
