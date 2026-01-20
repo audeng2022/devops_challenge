87870a41ed4ec8b9cf3066fa5b85f69273c1cbd7
audeng2022 <audeng2022@gmail.com>
2026-01-20 10:26:43 -0500

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,16 +6,10 @@ variable "aws_region"
   default = "us-east-1"
 }
 
-# Optional variable name to give resources created by Terraform
-variable "deployment_name" 
+# Optional variable name to give resources created by Terraform, default is devops-challenge
+variable "project_name" 
 {
   type    = string
   default = "devops-challenge"
 }
 
-# Restrict SSH, so give option to enter your IP, otherwise default to "1.2.3.4/32"
-variable "allowed_ssh_cidr" 
-{
-  type = string
-  default = "1.2.3.4/32"
-}
